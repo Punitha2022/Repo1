166f8e3861c8a338b62540a50104524a6e5dd623
Punitha2022 <108181725+Punitha2022@users.noreply.github.com>
2023-06-21 12:36:37 +0530

    Deleted the c variable declaration and added a new line

--- Sample.java
+++ Sample.java
@@ -2,7 +2,7 @@ public class Sample{
     public static void main(String args[]){
         int a=10;
         int b=20;
-        int c=a+b;
         System.out.println("Hello World");
+        System.out.println("a"+a + "b"+b);
     }
 }
