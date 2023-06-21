5282c88bf0fc7824f5408c80b657599d5eed264a
unknown <contactpunitha@yahoo.co.in>
2023-06-21 12:39:53 +0530

    Modified the value of a to 55

--- Sample.java
+++ Sample.java
@@ -1,6 +1,6 @@
 public class Sample{
     public static void main(String args[]){
-        int a=10;
+        int a=55;
         int b=20;
         System.out.println("Hello World");
         System.out.println("a"+a + "b"+b);
